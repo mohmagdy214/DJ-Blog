530b4e4657b2ae277831429df30814dd15dda42d
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-18 02:41:57 +0300

    add class based views

--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,33 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 
-def post_list(request):
-    data = Post.objects.all()
-    return render(request, 'post_list.html', {'posts':data})
 
 
-def post_details(request, post_id):
-    data = Post.objects.get(id=post_id)
-    return render(request, 'post_details.html', {'post':data})
+class PostList(ListView):    # template : post_list
+    model = Post             # contexe  : post_list , object_list
 
 
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-        return redirect('/blog')
-    else:
-        form = PostForm()
-    return render(request, 'new_post.html', {'form':form})
+
+class PostDetail(DetailView):
+    model = Post
 
 
-def edit_post(request, post_id):
-    data = Post.objects.get(id=post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=data)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-        return redirect('/blog')
-    else:
-        form = PostForm(instance=data)
-    return render(request, 'edit_post.html', {'form':form}) 
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','content','image','tags','create_date','draft','author']
+    success_url = '/blog'
 
 
-def delete_post(request, post_id):
-    data = Post.objects.get(id=post_id)
-    data.delete()
-    return redirect('/blog')
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title','content','image','tags','create_date','draft','author']
+    success_url = '/blog'
+    template_name = 'blog/edit_post.html'
+
+
+class PostDelete(DeleteView):
+    model = Post    
+    success_url = '/blog'
