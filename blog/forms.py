1c4437b552e9a9fd868fa60cd19d2ffb9b54cc53
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-24 13:29:10 +0300

    add the final touch

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,18 @@
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 from django import forms
 from .models import Post, Comment
-
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ("author",)
+        widgets = {
+            'content': SummernoteWidget(),
+        }
 
-
+        
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['comment']
+
--- project/urls.py
+++ project/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('blog/<int:pk>', post_detail),
     path('blog/<int:pk>/edit', PostUpdate.as_view()),
     path('blog/<int:pk>/delete', PostDelete.as_view()),
+    
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
